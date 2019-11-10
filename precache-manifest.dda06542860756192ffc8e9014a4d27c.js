aa14f261f593edc970b9d9f597e398aab9118992
hamzahalhariri <hamzah.alhariri@infomaker.se>
2019-11-10 14:07:52 +0100

    Updates

---
+++ precache-manifest.dda06542860756192ffc8e9014a4d27c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9933d193a429588faa3abfda18eecca3",
+    "url": "/15-15/index.html"
+  },
+  {
+    "revision": "4a2b079f47693623d804",
+    "url": "/15-15/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "36bdbbe28845242b9eb4",
+    "url": "/15-15/static/js/2.2137dd1e.chunk.js"
+  },
+  {
+    "revision": "4a2b079f47693623d804",
+    "url": "/15-15/static/js/main.6e7b4097.chunk.js"
+  },
+  {
+    "revision": "7b82543a01e95832d88b",
+    "url": "/15-15/static/js/runtime-main.315eb311.js"
+  }
+]);
